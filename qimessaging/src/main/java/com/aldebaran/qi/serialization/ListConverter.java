89064787752a9ede0e968cf379920cacd3e7fc01
Romain Vimont <rvimont@presta.aldebaran.com>
2016-05-09 14:17:31 +0200

    Make QiSerializer more flexible
    Instead of exposing a set of static methods, delegate the serialization
    to an interface "Converter".
    
    The serialization process iterates over the list of available
    converters, and use the first that can handle the conversion.
    
    Provide several default implementations, for lists, arrays, maps and
    structs (instances of classes having the annotation @QiStruct).
    
    The users can provide their own converters, that will be used if the
    default ones don't match.
    
    Change-Id: Ib836503163f3da535855543982316446954789e2
    Reviewed-on: http://gerrit.aldebaran.lan/72029
    Reviewed-by: rvimont <rvimont@presta.aldebaran.com>
    Tested-by: gerrit
    Reviewed-by: epinault <epinault@aldebaran-robotics.com>

---
+++ qimessaging/src/main/java/com/aldebaran/qi/serialization/ListConverter.java
@@ -0,0 +1,56 @@
+package com.aldebaran.qi.serialization;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aldebaran.qi.QiConversionException;
+
+public class ListConverter implements QiSerializer.Converter
+{
+
+  @Override
+  public boolean canSerialize(Object object)
+  {
+    return object instanceof List;
+  }
+
+  @Override
+  public List<?> serialize(QiSerializer serializer, Object object) throws QiConversionException
+  {
+    List<?> list = (List<?>) object;
+    List<Object> convertedList = new ArrayList<Object>();
+    for (Object item : list)
+    {
+      Object convertedItem = serializer.serialize(item);
+      convertedList.add(convertedItem);
+    }
+    return convertedList;
+  }
+
+  @Override
+  public boolean canDeserialize(Object object, Type targetType)
+  {
+    if (!(targetType instanceof ParameterizedType))
+      return false;
+
+    ParameterizedType parameterizedType = (ParameterizedType) targetType;
+    return List.class == parameterizedType.getRawType();
+  }
+
+  @Override
+  public List<?> deserialize(QiSerializer serializer, Object object, Type targetType) throws QiConversionException
+  {
+    List<?> list = (List<?>) object;
+    ParameterizedType parameterizedType = (ParameterizedType) targetType;
+    Type itemType = parameterizedType.getActualTypeArguments()[0];
+    List<Object> convertedList = new ArrayList<Object>();
+    for (Object item : list)
+    {
+      Object convertedItem = serializer.deserialize(item, itemType);
+      convertedList.add(convertedItem);
+    }
+    return convertedList;
+  }
+}
