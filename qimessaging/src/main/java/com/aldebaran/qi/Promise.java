2ff28ceb001924d61add4cfe3e9d5ccc8b162c6d
Qian JIN <qjin@presta.aldebaran.com>
2016-09-13 16:19:00 +0200

    Add class level javadoc for Future & Promise
    Part of the #36195.
    
    Change-Id: If61501e808f9d6e6d56cd69aca52283cc98713a9
    Reviewed-on: http://gerrit.aldebaran.lan/75207
    Reviewed-by: jsuchard <jsuchard@aldebaran.com>
    Tested-by: qjin <qjin@presta.aldebaran.com>
    Reviewed-by: tcruz <tcruz@aldebaran-robotics.com>
    Reviewed-by: qjin <qjin@presta.aldebaran.com>

--- qimessaging/src/main/java/com/aldebaran/qi/Promise.java
+++ qimessaging/src/main/java/com/aldebaran/qi/Promise.java
@@ -1,5 +1,19 @@
 package com.aldebaran.qi;
 
+/**
+ * Promise is a writable, single assignment container which sets the value of the {@link Future}.
+ *
+ * Promise and {@link Future} are two complementary concepts. They are designed to synchronise data
+ * between multiples threads. The {@link Future} holds the result of an asynchronous computation,
+ * from which you can retrieve the value of the result; the Promise sets the value of this computation,
+ * which resolves the associated {@link Future}.
+ *
+ * For promises that don't need a value and are just used to ensure correct ordering of asynchronous
+ * operations, the common pattern to use is {@link java.lang.Void} as a generic type.
+ *
+ * @param <T> The type of the result
+ */
+
 public class Promise<T>
 {
 
