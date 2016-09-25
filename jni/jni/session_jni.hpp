4a504f7481cb7e3a3db59cf74c541c6bd2f58dbe
Laurent LEC <llec@aldebaran.com>
2016-09-25 22:22:51 +0200

    Bind session.loadService
    Change-Id: I5d0cca11ff8b6cf64dc93bbaba793e891b0bae33
    Reviewed-on: http://gerrit.aldebaran.lan/75695
    Reviewed-by: llec <llec@aldebaran-robotics.com>
    Tested-by: llec <llec@aldebaran-robotics.com>

--- jni/jni/session_jni.hpp
+++ jni/jni/session_jni.hpp
@@ -25,6 +25,7 @@ extern "C"
   JNIEXPORT void JNICALL Java_com_aldebaran_qi_Session_onDisconnected(JNIEnv *env, jobject obj, jlong pSession, jstring callbackName, jobject objectInstance);
   JNIEXPORT void JNICALL Java_com_aldebaran_qi_Session_addConnectionListener(JNIEnv *env, jobject obj, jlong pSession, jobject listener);
   JNIEXPORT void JNICALL Java_com_aldebaran_qi_Session_setClientAuthenticatorFactory(JNIEnv *env, jobject obj, jlong pSession, jobject object);
+  JNIEXPORT void JNICALL Java_com_aldebaran_qi_Session_loadService(JNIEnv *env, jobject obj, jlong pSession, jstring moduleName);
 
 } // !extern "C"
 
