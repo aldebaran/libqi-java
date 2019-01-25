0caa3293bfcde24c96d6c1370bf8e2373cb19f95
Vincent Palancher <vincent.palancher@external.softbankrobotics.com>
2019-01-25 14:54:34 +0100

    jni.property: Uses ka::invoke_catch to make the code more robust.

--- jni/src/property.cpp
+++ jni/src/property.cpp
@@ -1,6 +1,7 @@
 #include "property.hpp"
 #include <qi/property.hpp>
 #include <qi/future.hpp>
+#include <ka/errorhandling.hpp>
 #include "jnitools.hpp"
 
 namespace
@@ -19,24 +20,51 @@ namespace
     }
     return valueType;
   }
+
+  /// Procedure<_(std::string)> Proc
+  template<typename Proc>
+  auto exceptionMessageHandler(Proc&& proc)
+    // TODO: Remove the trailing return type when we can switch to C++14
+    -> decltype(ka::compose(std::forward<Proc>(proc), ka::exception_message{}))
+  {
+    return ka::compose(std::forward<Proc>(proc), ka::exception_message{});
+  }
+
+  struct ThrowNewJavaException
+  {
+    JNIEnv* env;
+    void operator()(const std::string& msg) const
+    {
+      QI_ASSERT(env);
+      throwNewException(env, msg.c_str());
+    }
+  };
+
+  struct ThrowNewJavaExceptionReturn0
+  {
+    JNIEnv* env;
+    jlong operator()(const std::string& msg) const
+    {
+      QI_ASSERT(env);
+      throwNewException(env, msg.c_str());
+      return 0;
+    }
+  };
 }
 
 jlong JNICALL Java_com_aldebaran_qi_Property_createProperty(JNIEnv* env,
                                                             jclass QI_UNUSED(propertyClass),
                                                             jclass valueClass)
 {
-  try
-  {
-    auto* const valueType = getPropertyValueType(env, valueClass);
-    if (!valueType) return 0;
-    auto* const propertyManager = new PropertyManager(*valueType);
-    return reinterpret_cast<jlong>(propertyManager);
-  }
-  catch (std::exception& e)
-  {
-    throwNewException(env, e.what());
-  }
-  return 0;
+  return ka::invoke_catch(
+    exceptionMessageHandler(ThrowNewJavaExceptionReturn0{ env }),
+    [&]() -> jlong {
+      auto* const valueType = getPropertyValueType(env, valueClass);
+      if (!valueType)
+        return 0;
+      auto* const propertyManager = new PropertyManager(*valueType);
+      return reinterpret_cast<jlong>(propertyManager);
+    });
 }
 
 jlong JNICALL
@@ -45,44 +73,37 @@ Java_com_aldebaran_qi_Property_createPropertyWithValue(JNIEnv* env,
                                                        jclass valueClass,
                                                        jobject value)
 {
-  try
-  {
-    // TODO: Handle this case in the conversion between AnyValue and jobject.
-    if (env->IsSameObject(value, nullptr))
-    {
-      throwNewNullPointerException(env);
-      return 0;
-    }
+  return ka::invoke_catch(
+    exceptionMessageHandler(ThrowNewJavaExceptionReturn0{ env }),
+    [&]() -> jlong {
+      // TODO: Handle this case in the conversion between AnyValue and jobject.
+      if (env->IsSameObject(value, nullptr))
+      {
+        throwNewNullPointerException(env);
+        return 0;
+      }
 
-    auto* const  valueType = getPropertyValueType(env, valueClass);
-    if (!valueType) return 0;
-    auto* const propertyManager = new PropertyManager(*valueType, *env, value);
-    return reinterpret_cast<jlong>(propertyManager);
-  }
-  catch (std::exception& e)
-  {
-    throwNewException(env, e.what());
-  }
-  return 0;
+      auto* const valueType = getPropertyValueType(env, valueClass);
+      if (!valueType)
+        return 0;
+      auto* const propertyManager = new PropertyManager(*valueType, *env, value);
+      return reinterpret_cast<jlong>(propertyManager);
+    });
 }
 
 jlong JNICALL Java_com_aldebaran_qi_Property_get(JNIEnv* env,
                                                  jobject QI_UNUSED(propertyObj),
                                                  jlong pointer)
 {
-  try
-  {
-    auto propertyManager = reinterpret_cast<PropertyManager *>(pointer);
-    const auto& propertyPointer = propertyManager->property;
-    // Potential global reference issue here, due to multithreading, garbage collector and other fun.
-    auto futurePointer = new qi::Future<qi::AnyValue> { propertyPointer->value().async() };
-    return reinterpret_cast<jlong>(futurePointer);
-  }
-  catch (std::exception& e)
-  {
-    throwNewException(env, e.what());
-  }
-  return 0;
+  return ka::invoke_catch(
+    exceptionMessageHandler(ThrowNewJavaExceptionReturn0{ env }),
+     [&]() -> jlong {
+      auto propertyManager = reinterpret_cast<PropertyManager *>(pointer);
+      const auto& propertyPointer = propertyManager->property;
+      // Potential global reference issue here, due to multithreading, garbage collector and other fun.
+      auto futurePointer = new qi::Future<qi::AnyValue> { propertyPointer->value().async() };
+      return reinterpret_cast<jlong>(futurePointer);
+    });
 }
 
 jlong JNICALL Java_com_aldebaran_qi_Property_set(JNIEnv* env,
@@ -90,31 +111,24 @@ jlong JNICALL Java_com_aldebaran_qi_Property_set(JNIEnv* env,
                                                  jlong pointer,
                                                  jobject value)
 {
-  try
-  {
-    auto propertyManager = reinterpret_cast<PropertyManager*>(pointer);
-    propertyManager->setValue(env, value);
-  }
-  catch (std::exception& e)
-  {
-    throwNewException(env, e.what());
-  }
-  // Return a jlong to keep the compatibility with the Java side.
-  return 0;
+  return ka::invoke_catch(
+    exceptionMessageHandler(ThrowNewJavaExceptionReturn0{ env }),
+    [&]() -> jlong {
+      auto propertyManager = reinterpret_cast<PropertyManager*>(pointer);
+      propertyManager->setValue(env, value);
+      return 0;
+    });
 }
 
 void JNICALL Java_com_aldebaran_qi_Property_destroy(JNIEnv* env,
                                                     jobject QI_UNUSED(propertyObj),
                                                     jlong pointer)
 {
-  try
-  {
-    auto propertyManager = reinterpret_cast<PropertyManager*>(pointer);
-    propertyManager->destroy(env);
-    delete propertyManager;
-  }
-  catch (std::exception& e)
-  {
-    throwNewException(env, e.what());
-  }
+  ka::invoke_catch(
+    exceptionMessageHandler(ThrowNewJavaException{ env }),
+    [&] {
+      auto propertyManager = reinterpret_cast<PropertyManager*>(pointer);
+      propertyManager->destroy(env);
+      delete propertyManager;
+    });
 }
