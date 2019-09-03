5aa5f66a798357e83c0c2934ea7b9a58b34e47be
Julien BERNARD <julien.bernard@external.softbankrobotics.com>
2019-09-03 16:58:09 +0200

    Revert "java.UserTokenAuthenticator: Remove file."
    This reverts commit ddd836ca5b70440775e5c4e0cd335957b033d797.

---
+++ qimessaging/src/main/java/com/aldebaran/qi/UserTokenAuthenticator.java
@@ -0,0 +1,61 @@
+package com.aldebaran.qi;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Specific {@link ClientAuthenticator} that authenticates a user using a token.
+ */
+public class UserTokenAuthenticator implements ClientAuthenticator {
+    private final String user;
+    private final String token;
+
+    /**
+     * Constructs an authenticator with the specified user and token.
+     */
+    public UserTokenAuthenticator(@NotNull String user,@NotNull String token) {
+        this.user = Objects.requireNonNull(user);
+        this.token = Objects.requireNonNull(token);
+    }
+
+    /**
+     * Constructs an authenticator with the specified user and an empty token.
+     *
+     * @deprecated since 3.1.1, use the constructor that takes both user and token arguments
+     */
+    @Deprecated
+    public UserTokenAuthenticator(@NotNull String user) {
+        this(user, "");
+    }
+
+    @Override
+    public @NotNull Map<String, Object> initialAuthData() {
+        return new HashMap<String, Object>(){{
+            put("user", user);
+            put("token", token);
+        }};
+    }
+
+    @Override
+    public @NotNull Map<String, Object> _processAuth(Map<String, Object> authData) {
+        return new HashMap<>();
+    }
+
+    /**
+     * Indicates whether a new token has been retrieved during the last
+     * authentication.
+     *
+     * @return {@code false} all the time.
+     * @deprecated since 3.1.1, became useless since a new token cannot be retrieved with _processAuth.
+     */
+    @Deprecated
+    public boolean hasNewToken() {
+        return false;
+    }
+
+    public @NotNull String getToken() {
+        return token;
+    }
+}
